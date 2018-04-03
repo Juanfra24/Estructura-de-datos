f4c4f492463647fc13ad85ade16807b27ec8af70
juanfra2412 <jternicien@alumnos.uai.cl>
2018-04-03 12:05:26 -0300

    Tarea 4

--- Recursividad/busquedaBinaria.c
+++ Recursividad/busquedaBinaria.c
@@ -1,26 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 void swap(int* n1, int* n2);
 void bubble(int lista[],int n);
 void printArray(int arr[], int size);
+int calculoMitad(int largo);
+void busquedaBinaria(int lista[],int buscado,int mitad, int mitadInicial);
+
+int iterador= 0;
+
 
 int main(){
 
-	int lista[] = {12,3,0,53,24,17,88,36};
-	int largo = 8;
+	int lista[] = {3,6,2,1,4,8,5,7,9,10,11,12,13,14,15};
+	int largo = sizeof(lista)/sizeof(lista[0]);
+
+	int mitadInicial = calculoMitad(largo);
+
 
 	printArray(lista,largo);
 	bubble(lista, largo);
 	printArray(lista,largo);
 
+	busquedaBinaria(lista,1,calculoMitad(largo), mitadInicial);
+
     return 0;
 
 }
 
-void busquedaBinaria(){
-	
+void busquedaBinaria(int lista[],int buscado,int mitad,int mitadInicial){
+	if(buscado > lista[mitad-1]){
+		iterador+=1;
+		busquedaBinaria(lista, buscado, mitad + calculoMitad(mitadInicial),calculoMitad(mitadInicial));
+	}
+	if(buscado < lista[mitad-1]){
+		iterador+=1;
+		busquedaBinaria(lista, buscado, mitad - calculoMitad(mitadInicial),calculoMitad(mitadInicial));
+	}
+
+	if (buscado == lista[mitad-1])
+	{
+		printf("El numero %i fue encontrado en indice %i iteracion %i \n",mitad,mitad-1,iterador);
+		iterador = 0;
+	}
 }
+
+int calculoMitad(int largo){
+	if (largo % 2 == 0){
+		return (largo/2);
+	}
+	else{
+		return((largo+1)/2);
+	}
+
+}
+
 
 void swap(int* n1, int* n2){
 	int temp = *n1;
@@ -29,13 +64,24 @@ void swap(int* n1, int* n2){
 
 }
 void bubble(int lista[],int largo){
-	for(int i = 0; i< largo;i++){
-		for(int j=0; j-i-1;j++){
-			if(lista[j] > lista[j+1]){
-				swap(&lista[j],&lista[j+1]);
-			}
-		}
-	}	
+   int i, j;
+   int swapped;
+   for (i = 0; i < largo-1; i++)
+   {
+     swapped = 0;
+     for (j = 0; j < largo-1-i; j++)
+     {
+        if (lista[j] > lista[j+1])
+        {
+           swap(&lista[j], &lista[j+1]);
+           swapped = 1;
+        }
+     }
+ 
+     // IF no two elements were swapped by inner loop, then break
+     if (swapped == 0)
+        break;
+   }	
 
 
 }
